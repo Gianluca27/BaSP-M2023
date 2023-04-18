21ca63f7b4ab3763ace721ec6e0a30ff8eb31b9e
Gianluca Agrano <gianlucka1@gmail.com>
2023-04-18 19:52:52 -0300

    BaSP-W05-2023: Corrected Week-04 files

--- Week-05/excercises/01-variables-and-operators.js
+++ Week-05/excercises/01-variables-and-operators.js
@@ -2,16 +2,16 @@ console.log('--EXERCISE 1: VARIABLES AND OPERATORS');
 
 /*  a. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma
 de ambos números en una 3er variable. */
-console.log('Exercise 1.a');
 
+console.log('Exercise 1.a');
 var a = 25;
 var b = 13;
 var c = a + b;
 console.log(c);
 
 //   b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
-console.log('Exercise 1.b')
 
+console.log('Exercise 1.b')
 var name = 'Gianluca';
 var lastName = 'Agrano';
 var fullName = name + ' ' + lastName;
@@ -19,9 +19,9 @@ console.log(fullName);
 
 /*  c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
 guardando el resultado de la suma en una 3er variable (utilizar length). */
-console.log('Exercise 1.c')
 
+console.log('Exercise 1.c')
 var stringOne = 'Hola';
 var stringTwo = 'Adios';
-var stringThree = stringOne.length + stringTwo.length;
-console.log(stringThree);
+var stringAddition = stringOne.length + stringTwo.length;
+console.log(stringAddition);
--- Week-05/excercises/03-arrays.js
+++ Week-05/excercises/03-arrays.js
@@ -2,45 +2,47 @@ console.log('--EXERCISE 3: ARRAYS');
 
 /* a. Dado el siguiente array: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
 "Octubre", "Noviembre", "Diciembre"] mostrar por consola los meses 5 y 11 (utilizar console.log). */
-console.log('Exercise 3.a');
 
+console.log('Exercise 3.a');
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
 'September', 'October', 'November', 'December' ];
-console.log(months[4] + ' ' + months[10]);
+console.log(months[4] + ', ' + months[10]);
 
 /* b. Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort). */
-console.log('Exercise 3.b');
 
-console.log(months.sort());
+console.log('Exercise 3.b');
+monthsTwo = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+'September', 'October', 'November', 'December' ];
+console.log(monthsTwo.sort());
 
 /* c .Agregar un elemento al principio y al final del array (utilizar unshift y push). */
-console.log('Exercise 3.c');
 
+console.log('Exercise 3.c');
 months.unshift('Hello');
 months.push('World');
 console.log(months);
 
 /* d. Quitar un elemento del principio y del final del array (utilizar shift y pop). */
-console.log('Exercise 3.d');
 
+console.log('Exercise 3.d');
 months.shift();
 months.pop();
 console.log(months);
 
 /* e. Invertir el orden del array (utilizar reverse). */
-console.log('Exercise 3.e');
 
-months.reverse();
-console.log(months);
+console.log('Exercise 3.e');
+monthsThree = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+'September', 'October', 'November', 'December' ];
+console.log(monthsThree.reverse());
 
 /* f. Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join). */
-console.log('Exercise 3.f');
 
-var allMonths = months.join('-');
-console.log(allMonths);
+console.log('Exercise 3.f');
+console.log(months.join('-'));
 
 /* g. Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice). */
-console.log('Exercise 3.g');
 
-var monthsCopy = months.slice(4, 10);
+console.log('Exercise 3.g');
+var monthsCopy = months.slice(4, 11);
 console.log(monthsCopy);
